e36e0972c5da1c4478224fa67667521b933d3a22
Felix Izarra <felix_izarra@bose.com>
2021-10-01 21:58:41 -0500

    Swift Start Project

---
+++ Relatable/Relatable/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Relatable
+//
+//  Created by Felix Izarra on 10/1/21.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
